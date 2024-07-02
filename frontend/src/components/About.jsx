c6e5c776f60bfc211be4c072e4cbb6b12333d518
cloudwalker070 <abhinabdas2000@gmail.com>
2024-07-01 19:35:08 -0700

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        Welcome to RaveEvents , where we turn your event dreams into reality. With a passion for creativity and a commitment to excellence, we specialize in crafting unforgettable experiences that leave a lasting impression.
+        We offer a comprehensive range of event planning services tailored to meet your unique needs. Whether you’re planning a wedding, corporate event, birthday party, or any other special occasion, we’re here to handle every detail. 
+      </p>
+    </section>
+  );
+};
+
+export default About;
